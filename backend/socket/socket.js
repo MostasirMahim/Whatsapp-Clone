db0991e45882fd58bc90ea722a4321e7257f3782
Mostasir Mahim <mahimlive@gmail.com>
2024-10-11 17:41:32 +0600

    MERN Chat App

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://whatsup-mmco.onrender.com"],
     methods: ["GET", "POST"],
     credentials: true,
   },
